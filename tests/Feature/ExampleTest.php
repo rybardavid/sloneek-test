a1553710fe1be4a062316485066e1c82b94bfb96
Dávid Rybár <davidrybar123@gmail.com>
2025-04-18 20:39:53 +0200

    feat(tests): add RefreshDatabaseDoctrine trait to roll back database changes after each test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,15 +2,37 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\Support\Traits\RefreshDatabaseDoctrine;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabaseDoctrine;
+
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_the_application_returns_a_successful_one_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic test example.
+     */
+    public function test_the_application_returns_a_successful_two_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic test example.
+     */
+    public function test_the_application_returns_a_successful_three_response(): void
     {
         $response = $this->get('/');
 
---
+++ tests/Support/Traits/RefreshDatabaseDoctrine.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Support\Traits;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Tests\Support\Attributes\AfterTest;
+use Tests\Support\Attributes\BeforeTest;
+
+trait RefreshDatabaseDoctrine
+{
+
+    #[BeforeTest] public function beginTransaction(): void
+    {
+        app(EntityManagerInterface::class)
+            ->getConnection()
+            ->beginTransaction();
+    }
+
+    #[AfterTest] public function rollbackTransaction(): void
+    {
+        $em = app(EntityManagerInterface::class);
+        $conn = $em->getConnection();
+
+        if ($conn->isTransactionActive()) {
+            $conn->rollBack();
+        }
+
+        $em->clear();
+    }
+} 
